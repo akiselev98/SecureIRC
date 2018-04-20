b3076326d3fa630b720db331d1413cb5bc780bff
Alex Willis <willis62@purdue.edu>
2018-04-20 09:00:46 -0400

    SQLAlchemy part1

--- secureirc/__init__.py
+++ secureirc/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_session import Session
-from flask.ext.mysql import MySQL
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-mysql = MySQL(app);
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+from app import routes, models
+
 app.config['SECRET_KEY'] = 'aGVsbG8gZGFya25lc3MgbXkgb2xkIGZyaWVuZAo='
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
