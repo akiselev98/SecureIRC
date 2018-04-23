a09fbfaff327025f059cb11c93f13d5219c1f145
Alex Willis <willis62@purdue.edu>
2018-04-22 22:34:31 -0400

    A touch more database stuff

--- secureirc/models.py
+++ secureirc/models.py
@@ -1,12 +1,20 @@
 from secureirc import db
 
 class User(db.Model):
-   
+    __tablename__ = 'credentials'
     userName = db.Column(db.String(100), primary_key = True, index=True, unique=True)
     passWord = db.Column(db.String(128))
-    
+    #TODO: Hash passwords
     def __repr__(self):
         return '<User {}>'.format(self.userName)
 
 #TODO: Add rooms
 #something like room(db.Model) containing user objects
+
+class Room(db.Model)
+	#do not allow duplicate room names
+	roomName = db.Column(db.String(128), primary_key = True, index = True, unique = True)
+	
+
+	def __repr__(self):
+		#TODO
