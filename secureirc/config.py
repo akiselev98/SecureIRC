fecef3a03bc8677d347081dab68cce91eccf4043
Alex Willis <willis62@purdue.edu>
2018-04-24 06:06:19 -0400

    Now able to run with the SQL Server, read config.py in order to run on your local machine.

--- secureirc/config.py
+++ secureirc/config.py
@@ -3,7 +3,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
-   # SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1234@localhost:8324/data'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+     #                         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+    #***NOTE: The change the user and password below in order to connect to the SQL server
+
+
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://temp1:1234@127.0.0.1:3306/data'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
