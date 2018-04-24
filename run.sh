95d48207e0273773cc4a5ad735a0c9bf41d6c092
root <willis62@purdue.edu>
2018-04-24 05:39:45 +0000

    Tweaked run.sh so you have to choose the host to bind to

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn --worker-class eventlet --certfile ${FLASK_CERT_LOCATION} --keyfile ${FLASK_KEY} -b localhost:${FLASK_PORT} -w 1 secureirc:app
+gunicorn --worker-class eventlet --certfile ${FLASK_CERT_LOCATION} --keyfile ${FLASK_KEY} -b $1:${FLASK_PORT} -w 1 secureirc:app  
