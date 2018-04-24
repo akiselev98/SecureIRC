8ce63417db267b095bceb60ab4b95d2bd47f508c
Alex Willis <willis62@purdue.edu>
2018-04-24 05:37:33 -0400

    Database made with test data, still working on connecting with server

--- secureirc/config.py
+++ secureirc/config.py
@@ -5,4 +5,5 @@ class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
+   # SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1234@localhost:8324/data'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- secureirc/models.py
+++ secureirc/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     publickey = db.Column(db.String(256))
                               
     def __repr__(self):
-        return '<User {}>'.format(self.userName)
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
